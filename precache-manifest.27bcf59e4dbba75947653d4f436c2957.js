db592db26671f990f2d1fbba68ea755b8af25f95
marinona tests <maringaving@gmail.com>
2020-05-04 21:32:42 +0200

    Updates

---
+++ precache-manifest.27bcf59e4dbba75947653d4f436c2957.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7bb016e2a8400149d3369dfea1d6f78c",
+    "url": "/ina-puzzle/index.html"
+  },
+  {
+    "revision": "52ee35140ca542006dbc",
+    "url": "/ina-puzzle/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "0a6152bc17388fd61e9f",
+    "url": "/ina-puzzle/static/js/2.36d2cf4e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ina-puzzle/static/js/2.36d2cf4e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "52ee35140ca542006dbc",
+    "url": "/ina-puzzle/static/js/main.69473b25.chunk.js"
+  },
+  {
+    "revision": "70ce648f6d926aab3418",
+    "url": "/ina-puzzle/static/js/runtime-main.83ab417b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ina-puzzle/static/media/logo.5d5d9eef.svg"
+  }
+]);
