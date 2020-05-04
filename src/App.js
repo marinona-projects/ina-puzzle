c382ae35ec6347493a97e14bf3df9d846b05b6c5
marinona21 <marinona21@gmail.com>
2020-05-04 21:21:16 +0200

    init commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Ina 30</h1>
+      testing...
+    </div>
+  );
+}
+
+export default App;
